e6929f5b18ad1e587097b560b736557b159dbf56
César Hernández Cruz <japo@ciencias.unam.mx>
2021-03-31 11:05:54 -0600

    test: adds tests for the cycle generator
    Adds test for both the cycle generator and the cycle recognizer.
    
    For the cycle generator, it is checked that the adjacency matrix of the graph generated by calling `CycleMatrixGraph` corresponds to the adjacency matrix of an irreflexive cycle with the canonical circular ordering of its vertices.   Three cycles of random lengths between 3 and 1000 are constructed.
    
    For the cycle recognizer, the behaviour of the function `IsCycleMatrixGraph` is verified against hardcoded examples of cycle and non-cycle graphs.

--- pkg/generators/adjgenerators_test.go
+++ pkg/generators/adjgenerators_test.go
@@ -70,3 +70,132 @@ func TestCompleteMatrixGraph(t *testing.T) {
 		}
 	}
 }
+
+// TestIsCycleMatrixGraph calls IsCycleMatrixGraph with different hardcoded
+// graphs, including cycles of different lengths, disconnected 2-regular graphs,
+// and other non-cycle graphs.
+func TestIsCycleMatrixGraph(t *testing.T) {
+	a := [][]byte{
+		{0, 1, 0, 0, 0, 0, 0, 1},
+		{1, 0, 1, 0, 0, 0, 0, 0},
+		{0, 1, 0, 1, 0, 0, 0, 0},
+		{0, 0, 1, 0, 1, 0, 0, 0},
+		{0, 0, 0, 1, 0, 1, 0, 0},
+		{0, 0, 0, 0, 1, 0, 1, 0},
+		{0, 0, 0, 0, 0, 1, 0, 1},
+		{1, 0, 0, 0, 0, 0, 1, 0},
+	}
+	b := [][]byte{
+		{0, 1, 1},
+		{1, 0, 1},
+		{1, 1, 0},
+	}
+	c := [][]byte{
+		{0, 0, 1, 0, 0, 1, 0},
+		{0, 0, 0, 1, 0, 0, 1},
+		{1, 0, 0, 0, 1, 0, 0},
+		{0, 1, 0, 0, 0, 1, 0},
+		{0, 0, 1, 0, 0, 0, 1},
+		{1, 0, 0, 1, 0, 0, 0},
+		{0, 1, 0, 0, 1, 0, 0},
+	}
+	d := [][]byte{
+		{0, 1, 0, 1, 0, 0, 0, 0},
+		{1, 0, 1, 0, 0, 0, 0, 0},
+		{0, 1, 0, 1, 0, 0, 0, 0},
+		{1, 0, 1, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 1, 0, 1},
+		{0, 0, 0, 0, 1, 0, 1, 0},
+		{0, 0, 0, 0, 0, 1, 0, 1},
+		{0, 0, 0, 0, 1, 0, 1, 0},
+	}
+	e := [][]byte{
+		{0, 1, 0, 0, 0, 0, 0, 1},
+		{1, 0, 1, 0, 0, 0, 0, 0},
+		{0, 1, 0, 1, 0, 0, 0, 0},
+		{0, 0, 1, 1, 1, 0, 0, 0},
+		{0, 0, 0, 1, 0, 1, 0, 0},
+		{0, 0, 0, 0, 1, 0, 1, 0},
+		{0, 0, 0, 0, 0, 1, 0, 1},
+		{1, 0, 0, 0, 0, 0, 1, 0},
+	}
+	g := graph.NewMatrixGraph(a)
+	h := graph.NewMatrixGraph(b)
+	i := graph.NewMatrixGraph(c)
+	j := graph.NewMatrixGraph(d)
+	k := graph.NewMatrixGraph(e)
+	if !IsCycleMatrixGraph(g) {
+		t.Errorf(
+			"Expected %v, but got %v",
+			true,
+			IsCycleMatrixGraph(g),
+		)
+	}
+	if !IsCycleMatrixGraph(h) {
+		t.Errorf(
+			"Expected %v, but got %v",
+			true,
+			IsCycleMatrixGraph(h),
+		)
+	}
+	if !IsCycleMatrixGraph(i) {
+		t.Errorf(
+			"Expected %v, but got %v",
+			true,
+			IsCycleMatrixGraph(i),
+		)
+	}
+	if IsCycleMatrixGraph(j) {
+		t.Errorf(
+			"Expected %v, but got %v",
+			false,
+			IsCycleMatrixGraph(j),
+		)
+	}
+	if IsCycleMatrixGraph(k) {
+		t.Errorf(
+			"Expected %v, but got %v",
+			false,
+			IsCycleMatrixGraph(k),
+		)
+	}
+}
+
+// TestCycleMatrixGraph calls CycleMatrixGraph with five different
+// randomly generated numbers, and checks each of them to be a cycle by
+// exploring their adjacency matrices.
+func TestCycleMatrixGraph(t *testing.T) {
+	for i := 0; i < 5; i++ {
+		n := rand.Intn(1000)
+		if n > 2 {
+			c := CycleMatrixGraph(n)
+			d := c.DegreeSequence()
+			for _, v := range d {
+				if v != 2 {
+					t.Error("The graph is not 2-regular")
+				}
+			}
+			a := c.Adjacency()
+			if a[0][1] != 1 || a[0][n-1] != 1 {
+				t.Errorf(
+					"Adjacencies of vertex %v are not as expected",
+					0,
+				)
+			}
+			if a[n-1][0] != 1 || a[n-1][n-2] != 1 {
+				t.Errorf(
+					"Adjacencies of vertex %v are not as expected",
+					n-1,
+				)
+			}
+			for i := 1; i < n-1; i++ {
+				if a[i][i-1] != 1 || a[i][i+1] != 1 {
+					t.Errorf(
+						"Adjacencies of vertex %v are not as expected",
+						i,
+					)
+				}
+			}
+		}
+	}
+}
