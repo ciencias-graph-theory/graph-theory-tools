5e1a56d051536903e2330982f25c8c68584cd3d2
Victor <victor.z@ciencias.unam.mx>
2021-06-02 13:51:48 -0500

    perf: improves performance on sequence generator
    The improvement was made by not copying the slice at each step. Tests were
    modified accordingly.

--- internal/sequencegen/sequence_generator.go
+++ internal/sequencegen/sequence_generator.go
@@ -1,8 +1,8 @@
 // Package sequencegen provides functions for generating bool sequences.
 package sequencegen
 
-// makeInitial makes the initial sequence for the Weight function.
-func makeInitial(n, w int) []bool {
+// makeInitialW makes the initial sequence for the Weight function.
+func makeInitialW(n, w int) []bool {
 	b := make([]bool, n)
 	// First element is 01^w0^n-w-1
 	for i := 1; i <= w; i++ {
@@ -12,7 +12,7 @@ func makeInitial(n, w int) []bool {
 }
 
 // Weight returns a closure that acts as an iterator of binary sequences of size
-// n and weight w
+// n and weight w.
 func Weight(n, w int) func() []bool {
 	if w == 0 {
 		return func() []bool {
@@ -31,15 +31,12 @@ func Weight(n, w int) func() []bool {
 	init := true
 	x := 1
 	y := 0
-	b := makeInitial(n, w)
+	b := makeInitialW(n, w)
 	return func() []bool {
 		if init {
 			init = false
 			return b
 		}
-		b2 := make([]bool, n)
-		copy(b2, b)
-		b = b2
 		if x < n {
 			b[x] = false
 			b[y] = true
@@ -55,7 +52,58 @@ func Weight(n, w int) func() []bool {
 			}
 			return b
 		} else {
-			b = makeInitial(n, w)
+			b = makeInitialW(n, w)
+			x = 1
+			y = 0
+			return b
+		}
+	}
+}
+
+func makeInitialB(n int) []bool {
+	b := make([]bool, n)
+	// First element is 01^n-1
+	for i := 1; i < n; i++ {
+		b[i] = true
+	}
+	return b
+}
+
+// Binary returns a closure that acts as an iterator of binary sequences of size
+// n.
+func Binary(n int) func() []bool {
+	init := true
+	x := 1
+	y := 0
+	b := makeInitialB(n)
+	return func() []bool {
+		if init {
+			init = false
+			return b
+		}
+		b2 := make([]bool, n)
+		copy(b2, b)
+		b = b2
+		if y < n {
+			b[x] = false
+			b[y] = true
+			x++
+			y++
+			if x == n && y != n {
+				b[0] = false
+				y = 0
+				var bit int
+				if b[1] {
+					bit = 1
+				}
+				x = 0 + bit
+			} else if x != y && !b[x] {
+				b[x] = true
+				b[0] = false
+			}
+			return b
+		} else {
+			b = makeInitialB(n)
 			x = 1
 			y = 0
 			return b
--- internal/sequencegen/sequence_generator_test.go
+++ internal/sequencegen/sequence_generator_test.go
@@ -15,9 +15,10 @@ func contains(s [][]bool, e []bool) bool {
 }
 
 func TestWeight(t *testing.T) {
+	size := 3
 	// Test 1: weight 0
-	gen := Weight(3, 0)
-	res := make([]bool, 3)
+	gen := Weight(size, 0)
+	res := make([]bool, size)
 	got := gen()
 	if !reflect.DeepEqual(res, got) {
 		t.Errorf("Expected %v, got %v", res, got)
@@ -27,10 +28,11 @@ func TestWeight(t *testing.T) {
 		t.Errorf("Expected %v, got %v", res, got)
 	}
 	// Test 2: weight 1
-	got2 := make([][]bool, 3)
-	gen = Weight(3, 1)
-	for i := 0; i < 3; i++ {
-		got2[i] = gen()
+	got2 := make([][]bool, size)
+	gen = Weight(size, 1)
+	for i := 0; i < size; i++ {
+		got2[i] = make([]bool, size)
+		copy(got2[i], gen())
 	}
 	elem := []bool{true, false, false}
 	found := contains(got2, elem)
@@ -48,10 +50,11 @@ func TestWeight(t *testing.T) {
 		t.Errorf("%v is not in sequence %v", elem, got2)
 	}
 	// Test 3: weight 2
-	got3 := make([][]bool, 3)
-	gen = Weight(3, 2)
-	for i := 0; i < 3; i++ {
-		got3[i] = gen()
+	got3 := make([][]bool, size)
+	gen = Weight(size, 2)
+	for i := 0; i < size; i++ {
+		got3[i] = make([]bool, size)
+		copy(got3[i], gen())
 	}
 	elem = []bool{false, true, true}
 	found = contains(got3, elem)
@@ -69,7 +72,7 @@ func TestWeight(t *testing.T) {
 		t.Errorf("%v is not in sequence %v", elem, got3)
 	}
 	// Test 4: Weight 3
-	gen = Weight(3, 3)
+	gen = Weight(size, 3)
 	res = []bool{true, true, true}
 	got = gen()
 	if !reflect.DeepEqual(res, got) {
