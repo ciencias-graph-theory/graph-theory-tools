bcfffab185d73368a680fbfc771577b2103c988f
Mauricio Carrasco Ruiz <maucarrui@ciencias.unam.mx>
2022-08-31 12:45:50 -0500

    feat: Added EqualByteSlice function to compare two slices of bytes.

--- internal/sliceutils/sliceutils.go
+++ internal/sliceutils/sliceutils.go
@@ -18,6 +18,19 @@ func EqualIntSlice(a, b []int) bool {
 	return true
 }
 
+// This function checks whether two int slices are equal.
+func EqualByteSlice(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
 // This function checks whether two 2D byte slices are equal.
 func EqualByteMatrix(a, b [][]byte) bool {
 	if len(a) != len(b) {
