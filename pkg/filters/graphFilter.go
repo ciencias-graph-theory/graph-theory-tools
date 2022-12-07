ca97038f90d8e757176d63a3236dddddd456a760
Mauricio Carrasco Ruiz <maucarrui@ciencias.unam.mx>
2022-12-07 16:29:49 -0600

    feat: FilterGraph6 function implemented.
    Given an array of graph6 strings and a boolean function, FilterGraph6 returns a
    slice of graphs that satisfy the condition given by the boolean function.

---
+++ pkg/filters/graphFilter.go
@@ -0,0 +1,27 @@
+package filters
+
+import (
+	"github.com/ciencias-graph-theory/graph-theory-tools/pkg/formatters"
+	"github.com/ciencias-graph-theory/graph-theory-tools/pkg/graph"
+)
+
+type Graph = graph.Graph
+type StaticGraph = graph.StaticGraph
+type StaticDigraph = graph.StaticDigraph
+
+// Given an array of graph6 strings, return a list of the graphs that satisfy
+// the condition given by the boolean function.
+func FilterGraph6(strs []string, f func(*StaticGraph) bool) []*StaticGraph {
+	var sat []*StaticGraph
+
+	for _, s := range strs {
+		// Convert the string into a graph.
+		g := formatters.FromGraph6(s)
+
+		if f(g) {
+			sat = append(sat, g)
+		}
+	}
+
+	return sat
+}
