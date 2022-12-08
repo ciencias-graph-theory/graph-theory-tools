cc85b69cb63391daba690a661b3b53eaf2c36f88
Mauricio Carrasco Ruiz <maucarrui@ciencias.unam.mx>
2022-12-07 21:46:35 -0600

    feat: Added the rest of the filter functions.
    - Implemented FilterLoop6, FilterSparse6 and FilterDigraph6.
    
    - Created the function filterFormats that serves as a general purpose to filter
      any kind of strings that produce a StaticGraph structure.

--- pkg/filters/graphFilter.go
+++ pkg/filters/graphFilter.go
@@ -9,16 +9,56 @@ type Graph = graph.Graph
 type StaticGraph = graph.StaticGraph
 type StaticDigraph = graph.StaticDigraph
 
-// Given an array of graph6 strings and a boolean function, return a slice of
-// the graphs that satisfy the condition given by the boolean function.
-func FilterGraph6(strs []string, f func(*StaticGraph) bool) []*StaticGraph {
+// Given a slice of formatted graph strings and a boolean function (condition),
+// return a slice of the graphs that satisfy the condition given by the boolean
+// function.
+func filterFormats(strs []string, condition func(*StaticGraph) bool, formatting func(string) *StaticGraph) []*StaticGraph {
+	// Slice to keep the graphs that satisfy the condition.
 	var sat []*StaticGraph
 
 	for _, s := range strs {
-		// Convert the string into a graph.
-		g := formatters.FromGraph6(s)
+		// Convert the string into a graph using the formatting function.
+		g := formatting(s)
 
-		if f(g) {
+		// If the graph satisfies the condition, add it to the slice.
+		if condition(g) {
+			sat = append(sat, g)
+		}
+	}
+
+	return sat
+}
+
+// Given graph6 strings, return as a slice the ones that satisfy the given
+// condition.
+func FilterGraph6(strs []string, condition func(*StaticGraph) bool) []*StaticGraph {
+	return filterFormats(strs, condition, formatters.FromGraph6)
+}
+
+// Given loop6 strings, return as a slice the ones that satisfy the given
+// condition.
+func FilterLoop6(strs []string, condition func(*StaticGraph) bool) []*StaticGraph {
+	return filterFormats(strs, condition, formatters.FromLoop6)
+}
+
+// Given sparse6 strings, return as a slice the ones that satisfy the given
+// condition.
+func FilterSparse6(strs []string, condition func(*StaticGraph) bool) []*StaticGraph {
+	return filterFormats(strs, condition, formatters.FromSparse6)
+}
+
+// Given digraph6 strings, return as a slice the ones that satisfy the given
+// condition.
+func FilterDigraph6(strs []string, condition func(*StaticDigraph) bool) []*StaticDigraph {
+	// Slice to keep the digraphs that satisfy the condition.
+	var sat []*StaticDigraph
+
+	for _, s := range strs {
+		// Convert the string into a digraph using the formatting function.
+		g := formatters.FromDigraph6(s)
+
+		// If the digraph satisfies the condition, add it to the slice.
+		if condition(g) {
 			sat = append(sat, g)
 		}
 	}
