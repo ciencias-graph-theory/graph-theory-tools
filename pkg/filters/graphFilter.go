14e3fe09ce0098661e7ec8d5f2d9471fb48f5914
Mauricio Carrasco Ruiz <maucarrui@ciencias.unam.mx>
2022-12-08 17:23:26 -0600

    feat: Filter functions for files.
    - Added functions FilterGraph6FromFile, FilterLoop6FromFile,
      FilterSparse6FromFile, and FilterDigraph6FromFile.

--- pkg/filters/graphFilter.go
+++ pkg/filters/graphFilter.go
@@ -1,6 +1,7 @@
 package filters
 
 import (
+	"github.com/ciencias-graph-theory/graph-theory-tools/internal/ioutils"
 	"github.com/ciencias-graph-theory/graph-theory-tools/pkg/formatters"
 	"github.com/ciencias-graph-theory/graph-theory-tools/pkg/graph"
 )
@@ -64,4 +65,36 @@ func FilterDigraph6(strs []string, condition func(*StaticDigraph) bool) []*Stati
 	}
 
 	return sat
+}
+
+// Given a file containing graph6 strings and a boolean function, return the ones
+// that satisfy the condition given by the function as a graph slice.
+func FilterGraph6FromFile(filePath string, condition func(*StaticGraph) bool) []*StaticGraph {
+	graph6Strs, _ := ioutils.OpenFileAsStringSlices(filePath)
+
+	return FilterGraph6(graph6Strs, condition)
+}
+
+// Given a file containing loop6 strings and a boolean function, return the ones
+// that satisfy the condition given by the function as a graph slice..
+func FilterLoop6FromFile(filePath string, condition func(*StaticGraph) bool) []*StaticGraph {
+	loop6Strs, _ := ioutils.OpenFileAsStringSlices(filePath)
+
+	return FilterLoop6(loop6Strs, condition)
+}
+
+// Given a file containing sparse6 strings and a boolean function, return the
+// ones that satisfy the condition given by the function as a graph slice.
+func FilterSparse6FromFile(filePath string, condition func(*StaticGraph) bool) []*StaticGraph {
+	sparse6Strs, _ := ioutils.OpenFileAsStringSlices(filePath)
+
+	return FilterSparse6(sparse6Strs, condition)
+}
+
+// Given a file containing digraph6 strings and a boolean function, return the
+// ones that satisfy the condition given by the function as a digraph slice.
+func FilterDigraph6FromFile(filePath string, condition func(*StaticDigraph) bool) []*StaticDigraph {
+	digraph6Strs, _ := ioutils.OpenFileAsStringSlices(filePath)
+
+	return FilterDigraph6(digraph6Strs, condition)
 }
