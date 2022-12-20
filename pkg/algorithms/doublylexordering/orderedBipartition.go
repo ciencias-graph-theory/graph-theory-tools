79e93ab9d33ccfd2be67889178b312e60f310e9b
Mauricio Carrasco Ruiz <maucarrui@ciencias.unam.mx>
2022-12-20 15:14:10 -0600

    feat: New attribute and methods for ordered bipartition structure.

--- pkg/algorithms/doublylexordering/orderedBipartition.go
+++ pkg/algorithms/doublylexordering/orderedBipartition.go
@@ -27,8 +27,13 @@ type OrderedBipartition struct {
 
 	// Next partition in the order.
 	next *OrderedBipartition
+
+	// Indicates if its at the start of the ordered partition.
+	start bool
 }
 
+// NewOrderedBipartitionFromIntSet initializes an ordered bipartition with the
+// given set S.
 func NewOrderedBipartitionFromIntSet(S *IntSet) *OrderedBipartition {
 	return &OrderedBipartition{
 		set:            S,
@@ -37,9 +42,12 @@ func NewOrderedBipartitionFromIntSet(S *IntSet) *OrderedBipartition {
 		hasPartition:   false,
 		previous:       nil,
 		next:           nil,
+		start:          false,
 	}
 }
 
+// NewOrderedBipartitionFromIntSlice initializes an ordered bipartition with the
+// given values.
 func NewOrderedBipartitionFromIntSlice(s []int) *OrderedBipartition {
 	return &OrderedBipartition{
 		set:            set.NewIntSetFromValues(s),
@@ -48,35 +56,98 @@ func NewOrderedBipartitionFromIntSlice(s []int) *OrderedBipartition {
 		hasPartition:   false,
 		previous:       nil,
 		next:           nil,
+		start:          false,
 	}
 }
 
+// GetSet returns the set contained in the ordered bipartition.
 func (O *OrderedBipartition) GetSet() *IntSet {
 	return O.set
 }
 
+// SetPartition defines a bipartition for the ordered bipartition and respects
+// the previous order. Let P be the original partition and Pl and Pr the left
+// and right partition respectively. Then P's previous part is now Pl's previous
+// part, P's next part is now Pr's next part, Pl's next part is Pr and Pr's
+// previous part is Pl.
 func (O *OrderedBipartition) SetPartition(left, right *OrderedBipartition) {
+	// Define bipartition.
 	O.leftPartition = left
 	O.rightPartition = right
 	O.hasPartition = true
+
+	// Preserve the existing order.
+	previousPart := O.GetPrevious()
+	nextPart := O.GetNext()
+
+	previousPart.SetNext(left)
+	left.SetPrevious(previousPart)
+	left.SetNext(right)
+	right.SetPrevious(left)
+	right.SetNext(nextPart)
+	nextPart.SetPrevious(right)
+
+	// If the original part was at the start, then the left partition is now at
+	// the start.
+	if O.IsStart() {
+		left.SetStart()
+	}
 }
 
+// HasPartition returns if the ordered bipartition has a defined bipartition.
 func (O *OrderedBipartition) HasPartition() bool {
 	return O.hasPartition
 }
 
+// GetPrevious returns the part that goes before in the order, in other words,
+// the previous part.
 func (O *OrderedBipartition) GetPrevious() *OrderedBipartition {
 	return O.previous
 }
 
+// GetNext returns the part that goes next in the order.
 func (O *OrderedBipartition) GetNext() *OrderedBipartition {
 	return O.next
 }
 
+// SetPrevious defines the part that goes before in the order, in other words,
+// it defines the previous part.
 func (O *OrderedBipartition) SetPrevious(p *OrderedBipartition) {
 	O.previous = p
 }
 
+// SetNext defines the part that goes next in the order.
 func (O *OrderedBipartition) SetNext(n *OrderedBipartition) {
 	O.next = n
+}
+
+// SetStart indicates that this part is at the start of the order.
+func (O *OrderedBipartition) SetStart() {
+	O.start = true
+}
+
+// IsStart returns if the part is at the start of the order.
+func (O *OrderedBipartition) IsStart() bool {
+	return O.start
+}
+
+// GetOrderedPartitionAsSlice returns the ordered partition as an int slice.
+func GetOrderedPartitionAsSlice(start *OrderedBipartition) []int {
+	// Define a slice to save the order.
+	var order []int
+
+	// Traverse all the parts.
+	current := start
+
+	for current != nil {
+		// Append all the values in the set of the current part to the slice.
+		for _, val := range current.GetSet().GetValues() {
+			order = append(order, val)
+		}
+
+		// Move to the next part.
+		current = current.GetNext()
+	}
+
+	return order
 }
