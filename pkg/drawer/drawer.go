c134557207ed1d18127de4c55ddfd4595bf99b7e
Alan Arteaga <alanarteagav@ciencias.unam.mx>
2023-03-01 22:05:08 -0600

    feat: Adjacency matrix graph drawer implementation

--- pkg/drawer/drawer.go
+++ pkg/drawer/drawer.go
@@ -30,21 +30,28 @@ func Circular(g graph.Graph, width int, height int) *svg.Svg {
 	h := float64(height)
 	s := svg.NewSvg(width+40, height+40)
 	d := math.Min(w, h)
-
 	cx := 10 + (w / 2)
 	cy := 10 + (h / 2)
 	r := d / 2
 
-	var vertexRadius float64
-	vertexRadius = math.Sin(float64(math.Pi)/(2.0*float64(g.Order()))) * r
-
-	//angleDivision := 360.0 / float64(g.Order()) // fix graph order function
-
-	l, _ := g.List()
-	angleDivision := 360.0 / float64(len(l))
+	order := float64(g.Order())
+	vertexRadius := math.Sin(float64(math.Pi)/(2.0*order)) * r
+	angleDivision := 360.0 / order
 	angle := 0.0
 
-	if list, err := g.List(); err != graph.NilAdjacencyList {
+	if matrix, err := g.Matrix(); err != graph.NilAdjacencyMatrix {
+		for i, v := range matrix {
+			x := cx - (r * math.Sin(math.Pi*2*angle/360))
+			y := cy - (r * math.Cos(math.Pi*2*angle/360))
+			angle += angleDivision
+			verticesMap[i] = vertex{x, y, "white"}
+			for j, n := range v {
+				if n == 1 {
+					edgesSlice = append(edgesSlice, edge{i, j})
+				}
+			}
+		}
+	} else if list, err := g.List(); err != graph.NilAdjacencyMatrix {
 		for v, neighbours := range list {
 			x := cx - (r * math.Sin(math.Pi*2*angle/360))
 			y := cy - (r * math.Cos(math.Pi*2*angle/360))
@@ -54,22 +61,21 @@ func Circular(g graph.Graph, width int, height int) *svg.Svg {
 				edgesSlice = append(edgesSlice, edge{v, u})
 			}
 		}
-		for _, e := range edgesSlice {
-			u := verticesMap[e.u]
-			v := verticesMap[e.v]
-			x1 := u.cx
-			y1 := u.cy
-			x2 := v.cx
-			y2 := v.cy
-			s.DrawLine(x1, y1, x2, y2, "black", 1)
-		}
-		for _, info := range verticesMap {
-			s.DrawCircle(info.cx, info.cy, vertexRadius, "black", 1, info.color)
-		}
+	}
+	for _, e := range edgesSlice {
+		u := verticesMap[e.u]
+		v := verticesMap[e.v]
+		x1 := u.cx
+		y1 := u.cy
+		x2 := v.cx
+		y2 := v.cy
+		s.DrawLine(x1, y1, x2, y2, "black", 1)
+	}
+	for _, info := range verticesMap {
+		s.DrawCircle(info.cx, info.cy, vertexRadius, "black", 1, info.color)
 	}
 	// Refactor this!
 	data := []byte(s.Content())
 	writer.Write("test.svg", data)
-
 	return s
 }
